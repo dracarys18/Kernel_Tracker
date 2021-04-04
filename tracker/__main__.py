03cb544274fa1a051bd01395909ca7fb1e1e0684
dracarys18 <karthihegde010@gmail.com>
2021-04-04 14:59:50 +0530

    Embed the repo link instead of kernel.org
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- tracker/__main__.py
+++ tracker/__main__.py
@@ -5,9 +5,15 @@ from tracker import lin
 def main():
     updatables = lin.updatable_kernels()
     client.parse_mode = 'html'
+    href=''
     if updatables:
         for i in updatables:
-            client.send_message(-1001195071888,"<strong>New Kernel version available</strong>\n<a href='https://www.kernel.org/'>{0}</a>".format(i))
+            if str(i).startswith("next"):
+                href='https://www.kernel.org/'
+            else:
+                href='https://git.kernel.org/torvalds/h/{0}'.format(str(i))
+
+            client.send_message(-1001195071888,"<strong>New Kernel version available</strong>\n<a href='{0}'>{1}</a>".format(href,i))
 
 if __name__=="__main__":
     bot_token=str(os.getenv("BOT_TOKEN"))
